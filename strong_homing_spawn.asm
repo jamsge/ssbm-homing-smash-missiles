f3d8a71731a72dd66f978538eae25e99729e74c7
jams <36876904+jamsge@users.noreply.github.com>
2023-09-12 23:03:27 -0700

    complete placing spawn entity in free memory for both p1 and p2

--- strong_homing_spawn.asm
+++ strong_homing_spawn.asm
@@ -6,13 +6,13 @@ backup
 mr r31, r3 
 lwz r30, 0x2c(r31) # put entity data in r30
 load r29, 0x803fc420 # area with free memory
-stw r30, 0x0(r29) # set shared mem to entity data of item
 
 lwz r27, 0x518(r30) # get pointer to owner player struct entity
 lwz r27, 0x2c(r27) # get pointer to entity data
 lbz r27, 0x0c(r27) # 0x0c is the player slot, 0 indexed
-# mulli r27, r27, 8 # shared address pointer + counter takes up 8 bytes
-# add r29, r27, r29 # offset free memory address by 8*zeroIndexedPlayerSlot
+mulli r27, r27, 8 # shared address pointer + counter takes up 8 bytes
+add r29, r27, r29 # offset free memory address by 8*zeroIndexedPlayerSlot
+stw r30, 0x0(r29) # set shared mem to entity data of item
 
 
 lwz r28, 0x4(r29) # get shared counter, alternates 0 and 1 on each. make one for each player?
